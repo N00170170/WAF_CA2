6002a3769b4dca2543d3db7e4684aed5ddb6cd44
N00170170 <N00170170@users.noreply.github.com>
2020-03-28 18:42:55 +0000

    Initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    This is app vue
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
